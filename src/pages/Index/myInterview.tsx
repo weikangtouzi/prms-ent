40fdb859580542a725e9e075944e4e027fd79659
卫正明 <weizhengming@xiaomi.com>
2021-11-11 00:54:37 +0800

    add myinterview

--- src/pages/Index/myInterview.tsx
+++ src/pages/Index/myInterview.tsx
@@ -1,8 +1,8 @@
-import {Button, Radio, Result, Table} from "antd";
-import {useState} from "react";
+import { Button, Radio, Result, Table } from 'antd';
+import { useState } from 'react';
 
-const MyInterview = ()=>{
-  const [data] = useState([])
+const MyInterview = () => {
+  const [data] = useState([]);
   const columns = [
     {
       title: '面试时间',
@@ -17,26 +17,27 @@ const MyInterview = ()=>{
       dataIndex: 'address',
     },
   ];
-  return <div>
-    <Radio.Group defaultValue="a">
-      <Radio.Button value="a">今天</Radio.Button>
-      <Radio.Button value="b">明天</Radio.Button>
-      <Radio.Button value="c">一周内</Radio.Button>
-    </Radio.Group>
-    <div style={{marginTop:'20px'}}>
-      {
-        data.length>0 ?
-          <Table size={'small'} columns={columns} dataSource={data} />:
+  return (
+    <div>
+      <Radio.Group defaultValue="a">
+        <Radio.Button value="a">今天</Radio.Button>
+        <Radio.Button value="b">明天</Radio.Button>
+        <Radio.Button value="c">一周内</Radio.Button>
+      </Radio.Group>
+      <div style={{ marginTop: '20px' }}>
+        {data.length > 0 ? (
+          <Table size={'small'} columns={columns} dataSource={data} />
+        ) : (
           <Result
-            icon={<img src={'/images/empty_1.png'} alt='no interview' style={{width:'30%'}}/>}
+            icon={<img src={'/images/empty_1.png'} alt="no interview" style={{ width: '180px' }} />}
             title="今天还没有安排面试哦～"
             subTitle=""
             extra={<Button type="link">快去挑选心仪的人才</Button>}
           />
-      }
-
+        )}
+      </div>
     </div>
-  </div>
-}
+  );
+};
 
 export default MyInterview;
